55c07041b273d9b88b4c58be110567a65bf7ab60
unknown <guruluckystacker@gmail.com>
2021-12-22 03:08:06 -0300

    payment, done page

--- server.js
+++ server.js
@@ -21,14 +21,14 @@ app.use('/api/tickets', require('./routes/api/tickets'));
 app.use('/api/stripe', require('./routes/api/stripe'));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-}
+// }
 
 const PORT = process.env.PORT;
 
